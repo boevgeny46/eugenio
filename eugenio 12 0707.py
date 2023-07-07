581c6ec147cb68dac685f1cc3691c14eedd146fc
Eugenio <eugenekrylova@yandex.ru>
2023-07-07 12:01:36 +0300

    достаем базу данных qlite3

---
+++ eugenio 12 0707.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+# подключаемся к нашей базе данных
+connection = sqlite3.connect('films_db.sqlite')
+
+# оздаем курсор и через курсор созаем запросы
+cursor = connection.cursor()
+
+# запрос по доставке информации  fetchall команда доставить все запрошенные по запросу элементы
+# fetchone- доставит только первый элемент
+#  fetchmany(N) - выдаст только N первых элементов
+result = cursor.execute("""SELECT * FROM films WHERE year =? and duration > ?""", (2010, 90)).fetchall()
+
+# вывод информации на экран:
+for item in result:
+    print(item)
+# отключаемся от базы данных (disconnect)
+connection.close()
+
+# программа работает
