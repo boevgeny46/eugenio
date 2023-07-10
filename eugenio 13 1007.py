fa59004c058a4968a88365a79d08f384f74f3c1d
Eugenio <eugenekrylova@yandex.ru>
2023-07-10 13:12:05 +0300

    flask continue 3 не работает

--- eugenio 13 1007.py
+++ eugenio 13 1007.py
@@ -4,7 +4,7 @@
 
 #  это стартовая версия приложения FLASK каркасный вариант по правилам pycharm
 
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)  # create application -приложение
 
@@ -13,6 +13,48 @@ app = Flask(__name__)  # create application -приложение
 @app.route('/index')
 def index():
     return 'Admiral<br><a href="/slogan">Слоган</a'
+@app.route('/poster')
+def poster():
+    < !DOCTYPE
+    html >
+    < html
+    lang = "en" >
+    < head >
+    < meta
+    charset = "UTF-8" >
+    < title > poster < / title >
+    link
+    rel = "stylesheet"
+    href = "https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+    integrity = "sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N"
+    crossorigin = "anonymous" >
+
+    < script
+    src = "https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
+    integrity = "sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+    crossorigin = "anonymous" > < / script >
+    < script
+    src = "https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"
+    integrity = "sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct"
+    crossorigin = "anonymous" > < / script >
+
+< / head >
+< body >
+< h1 > Постер
+к
+фильму < / h1 >
+< img
+src = "{url_for('static', filename='images/admiral.png)}"
+
+</head>
+<body>
+< h1 class= > Постер k фильму</h1
+
+< img
+src = "{url_for('static', filename='images/admiral.png)}"
+
+< / body >
+< / html >
 
 @app.route('/slogan')
 def slogan():
