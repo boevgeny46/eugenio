152f5163e33357ada1cf18ae8d96783c4de57a3e
Eugenio <eugenekrylova@yandex.ru>
2023-07-10 12:04:52 +0300

    flask continue 2

--- eugenio 13 1007.py
+++ eugenio 13 1007.py
@@ -1,16 +1,32 @@
 # ('/other') -  -от корня (root)
 # ('./other') -от текущей директории
+
+
+#  это стартовая версия приложения FLASK каркасный вариант по правилам pycharm
+
 from flask import Flask
+
 app = Flask(__name__)  # create application -приложение
 
-@app.route('/')
+
+@app.route('/')      # это два декоратора они пишутся вместе
 @app.route('/index')
 def index():
-    return "Hello"
+    return 'Admiral<br><a href="/slogan">Слоган</a'
 
-if  __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000)  # указываем хост  и порт
+@app.route('/slogan')
+def slogan():
+
+    return 'ибо крепка как смерть любовь<br><a href="/">Назад/a'
+
+@app.route('/coundown')
+def countdown():
+    lst = [str(x) for x in range(10,0, -1)]
+    lst.append('Start!!!')
+    return '<br>'.join(lst)
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000)  # указываем хост (виртуальный сервер и порт)
 
 #  это стартовая версия приложения FLASK
-
-
