3bda5dbd6623e2eed9d2a487539a17404a1af197
Eugenio <eugenekrylova@yandex.ru>
2023-07-11 14:36:33 +0300

    ffzz

--- eugenio 16 1107.py
+++ eugenio 16 1107.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask import render_template
+import json
+
 app = Flask(__name__)  # create application -приложение
 
 
-@app.route('/')      # это два декоратора они пишутся вместе
+@app.route('/')  # это два декоратора они пишутся вместе
 @app.route('/index')
 def index():
     param = {}
@@ -12,10 +14,26 @@ def index():
 
     return render_template('index.html', **param)
 
+
 @app.route('/odd_even')
 def odd_even():
     return render_template('odd_even', **param)
 
 
+@app.route('/news')
+def news():
+    with open('news.json', 'rt', encoding='utf-8') as f:
+        news_list = json.loads(f.read())
+        return render_template('news.html', title='Новости', news=news_list)
+        print(news_list)
+
+    lst = ['ANN', 'TOM', 'BOB']
+    return render_template('news.html', title="FOR", news=lst)
+
+@app.route('/vartest')
+def vartest():
+    return render_template('var_test.html', title='Переменные в HTML')
+
+
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000)  # указываем хост (виртуальный сервер и порт)
+    app.run(host='127.0.0.1', port=5000, debug=True)  # указываем хост (виртуальный сервер и порт)
